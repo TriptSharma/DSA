e31b2961f40095b2ff131f9f2e46cb1b3c7e5c90
TriptSharma <tript35.sharma@gmail.com>
2019-09-12 02:48:04 +0530

    push tower of Hanoi soln

--- recursion/towerOfHanoi.cpp
+++ recursion/towerOfHanoi.cpp
@@ -5,11 +5,18 @@ void towerOfHanoi(int n, char src, char dst, char helper){
     //base case
     if(n==0)
         return;
-    
+    //recursive call (It shifs n-1 disk to helper; stacks up all n-1 disks to helper)
+    towerOfHanoi(n-1, src, helper, dst);
+
+    //move current disk to destination
+    cout<<"Move disk "<<n<<" from "<<src<<" to "<<dst<<endl;
+
+    //put n-1 disk to destination
+    towerOfHanoi(n-1, helper, dst, src);
 }
 int main(){
     int n;  //no of disks
     cin>>n;
-    void towerOfHanoi(n, 'A', 'B', 'C');
-
+    void towerOfHanoi(n, "A", "B", "C");
+    return 0;
 }
